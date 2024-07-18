e3712a807e0662c6d98b6b6a67c590f64aea38d5
Keval Dharmeshbhai Gandevia <kv476075@dal.ca>
2024-07-18 00:19:15 -0300

    Feat: add all required model for project

--- backend/src/main/java/com/tiffinbox/backend/models/User.java
+++ backend/src/main/java/com/tiffinbox/backend/models/User.java
@@ -1,4 +1,32 @@
 package com.tiffinbox.backend.models;
 
+import com.tiffinbox.backend.utils.UserRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = {"customer", "foodServiceProvider", "order", "review"})
 public class User {
+    @Id
+    public String userId;
+    public String email;
+    public String password;
+    public Boolean isAdminVerified;
+    @Enumerated(EnumType.STRING)
+    public UserRole userRole;
+    @OneToOne
+    public Customer customer;
+    @OneToOne
+    public FoodServiceProvider foodServiceProvider;
+    @OneToMany
+    public Order order;
+    @OneToMany
+    public Review review;
 }
