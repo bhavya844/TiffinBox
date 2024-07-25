8227289b06565ae4049d8dcd7695a8a2895030f0
Keval Dharmeshbhai Gandevia <kv476075@dal.ca>
2024-07-24 22:40:39 -0300

    Feat: add cors config to allow requests from frontend and install axios for integration of frontend and backend

--- backend/src/main/java/com/tiffinbox/backend/configurations/ApplicationConfiguration.java
+++ backend/src/main/java/com/tiffinbox/backend/configurations/ApplicationConfiguration.java
@@ -14,6 +14,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -46,5 +48,25 @@ public class ApplicationConfiguration {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    /**
+     * cors configuration
+     *
+     * @return - cors registry object
+     */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
 
 }
