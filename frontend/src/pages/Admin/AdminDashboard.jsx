6ad4de9d4ececc1f5668193377b075bb98a1e9a6
Keval Dharmeshbhai Gandevia <kv476075@dal.ca>
2024-07-27 19:16:58 -0300

    Feat: implement frontend of admin dashboard

---
+++ frontend/src/pages/Admin/AdminDashboard.jsx
@@ -0,0 +1,35 @@
+
+const AdminDashboard = () => {
+  return (
+    <div className="container mx-auto px-6 py-6">
+      <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-3 justify-stretch">
+        <div className="card bg-base-100 shadow-md">
+          <div className="card-body">
+            <h1 className="card-title text-3xl">
+              <p className="text-center">Total Users</p>
+            </h1>
+            <p className="font-semibold text-center">295</p>
+          </div>
+        </div>
+        <div className="card w-50 bg-base-100 shadow-md">
+          <div className="card-body">
+            <h1 className="card-title text-3xl">
+              <p className="text-center">Total Orders</p>
+            </h1>
+            <p className="font-semibold text-center">295</p>
+          </div>
+        </div>
+        <div className="card w-50 bg-base-100 shadow-md">
+          <div className="card-body">
+            <h1 className="card-title text-3xl">
+              <p className="text-center">Total Earnings</p>
+            </h1>
+            <p className="font-semibold text-center">$99999</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminDashboard;
